b6ed16200ae3563047bcf8a4c1d9104f23144678
danyllo <danyllopenna10@gmail.com>
2019-06-13 10:04:29 -0300

    commit inicial do INFOCITY

---
+++ js/js.js
@@ -0,0 +1,28 @@
+
+/* checar nome e senha */
+function acesso(formulario1){
+location = "cadastro.html"
+}
+
+function alerta(alerta){
+    
+    location = "formulario.html"
+}
+
+function logar(){
+    if (formulario.nome.value == "pedro" && formulario.cpf.value == "12345678901") {
+        location = "interacao.html"
+    } else {
+    
+    }
+}
+
+function envio(){
+    
+    location = "topicos.html"
+}
+
+function respondendo(){
+    
+    location = "topicos.html"
+}
